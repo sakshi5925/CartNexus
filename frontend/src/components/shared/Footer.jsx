aa93d699521df3568535a6a86aa2d463b1532fa2
sakshi5925 <152876445+sakshi5925@users.noreply.github.com>
2025-03-12 13:26:19 +0900

    footercss

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -5,18 +5,18 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div>
-    <div>
-      <div>
+    <>
+    <div className='flex justify-between '>
+      <div className=''>
         <h2>Explore</h2>
-        <ul>
+        <ul className='list-none'>
             <li>Search</li>
             <li>About Us</li>
         </ul>
       </div>
       <div>
         <h2>About</h2>
-        <ul>
+        <ul className='list-none'>
             <li>Delivery</li>
             <li>Returns</li>
             <li>Terms</li>
@@ -25,7 +25,7 @@ const Footer = () => {
       </div>
       <div>
         <h2>Connect</h2>
-        <ul>
+        <ul className='list-none flex gap-x-4'>
             <li><RiFacebookFill /></li>
             <li><FaTwitter /></li>
             <li><FaLinkedinIn /></li>
@@ -33,11 +33,8 @@ const Footer = () => {
         </ul>
       </div>
     </div>
-    <div>
-        <div></div>
-        <div></div>
-    </div>
-    </div>
+
+    </>
   )
 }
 
